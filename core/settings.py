ffbe82a70b88ea8e2cf7d3f75b2e58744b1f3c41
momin <tursunov545@gmai.com>
2026-08-03 00:50:07 +0500

    qoshildi

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-6xs=20@@rwy-y#ix)#j8z(da_utwdes%$o%!jj$&!riqg6wq%%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
